e74d4025b92026501f7e35e0a4de0bb085973a47
InitiativeMarketingGroup <initiativemarketinggroup@gmail.com>
2025-12-20 17:55:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
-const CACHE_NAME = "asvaye-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/manifest.json",
-  "https://raw.githubusercontent.com/InitiativeMarketingGroup/As-Vaye_PWA/main/As-Vaye_logo_logo.2.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("asvaye-cache").then((cache) => cache.addAll([
+      "/index.html",
+      "/app.js",
+      "/manifest.json"
+    ]))
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
